6a9d557c9f18fafd49abd267f1727382551aa5cc
Chakkapat singhanuwattana <51953747+tanguy1842@users.noreply.github.com>
2022-05-12 15:18:21 +0700

    good

---
+++ bmi_6206022610017_ms/android/app/src/main/kotlin/com/example/bmi_6206022610017_ms/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmi_6206022610017_ms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bmi_6206022610017_ms/lib/main.dart
@@ -0,0 +1,418 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'BMI',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // text fields' controllers
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _lastnameController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weighController = TextEditingController();
+
+  final CollectionReference _bmi = FirebaseFirestore.instance.collection('bmi');
+
+  // This function is triggered when the floatting button or one of the edit buttons is pressed
+  // Adding a product if no documentSnapshot is passed
+  // If documentSnapshot != null then update an existing product
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _nameController.text = documentSnapshot['name'];
+      _lastnameController.text = documentSnapshot['lastname'];
+      _genderController.text = documentSnapshot['gender'];
+      _ageController.text = documentSnapshot['age'].toString();
+      _heightController.text = documentSnapshot['height'].toString();
+      _weighController.text = documentSnapshot['weigh'].toString();
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                // prevent the soft keyboard from covering text fields
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'name'),
+                ),
+                TextField(
+                  controller: _lastnameController,
+                  decoration: const InputDecoration(labelText: 'lastname'),
+                ),
+                TextField(
+                  controller: _genderController,
+                  decoration: const InputDecoration(labelText: 'gender'),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: _ageController,
+                  decoration: const InputDecoration(
+                    labelText: 'age',
+                  ),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: _weighController,
+                  decoration: const InputDecoration(
+                    labelText: 'weigh(KG)',
+                  ),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: _heightController,
+                  decoration: const InputDecoration(
+                    labelText: 'height(CM)',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text(action == 'create' ? 'Create' : 'Update'),
+                  onPressed: () async {
+                    final String? name = _nameController.text;
+                    final String? lastname = _lastnameController.text;
+                    final String? gender = _nameController.text;
+                    final double? age = double.tryParse(_ageController.text);
+                    final double? height =
+                        double.tryParse(_heightController.text);
+                    final double? weigh =
+                        double.tryParse(_weighController.text);
+                    if (name != null &&
+                        lastname != null &&
+                        gender != null &&
+                        age != null &&
+                        height != null &&
+                        weigh != null) {
+                      if (action == 'create') {
+                        // Persist a new product to Firestore
+                        await _bmi.add({
+                          "name": name,
+                          "lastname": lastname,
+                          "gender": gender,
+                          "age": age,
+                          "height": height,
+                          "weigh": weigh
+                        });
+                      }
+
+                      if (action == 'update') {
+                        // Update the product
+                        await _bmi.doc(documentSnapshot!.id).update({
+                          "name": name,
+                          "lastname": lastname,
+                          "gender": gender,
+                          "age": age,
+                          "height": height,
+                          "weigh": weigh
+                        });
+                      }
+
+                      // Clear the text fields
+                      _nameController.text = '';
+                      _nameController.text = '';
+                      _lastnameController.text = '';
+                      _genderController.text = '';
+                      _ageController.text = '';
+                      _heightController.text = '';
+                      _weighController.text = '';
+                      // Hide the bottom sheet
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  // Deleteing a product by id
+  Future<void> _deleteProduct(String productId) async {
+    await _bmi.doc(productId).delete();
+
+    // Show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted a product')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI'),
+      ),
+      // Using StreamBuilder to display all products from Firestore in real-time
+      body: StreamBuilder(
+        stream: _bmi.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(documentSnapshot['name']),
+                    subtitle: Text(documentSnapshot['weigh'].toString()),
+                    trailing: SizedBox(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          // Press this button to edit a single product
+                          IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () =>
+                                  _createOrUpdate(documentSnapshot)),
+                          // This icon button is used to delete a single product
+                          IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () =>
+                                  _deleteProduct(documentSnapshot.id)),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      // Add new product
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createOrUpdate(),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       // Remove the debug banner
+//       debugShowCheckedModeBanner: false,
+//       title: 'Kindacode.com',
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   // text fields' controllers
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _priceController = TextEditingController();
+
+//   final CollectionReference _productss =
+//       FirebaseFirestore.instance.collection('products');
+
+//   // This function is triggered when the floatting button or one of the edit buttons is pressed
+//   // Adding a product if no documentSnapshot is passed
+//   // If documentSnapshot != null then update an existing product
+//   Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+//     String action = 'create';
+//     if (documentSnapshot != null) {
+//       action = 'update';
+//       _nameController.text = documentSnapshot['name'];
+//       _priceController.text = documentSnapshot['price'].toString();
+//     }
+
+//     await showModalBottomSheet(
+//         isScrollControlled: true,
+//         context: context,
+//         builder: (BuildContext ctx) {
+//           return Padding(
+//             padding: EdgeInsets.only(
+//                 top: 20,
+//                 left: 20,
+//                 right: 20,
+//                 // prevent the soft keyboard from covering text fields
+//                 bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+//             child: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 TextField(
+//                   controller: _nameController,
+//                   decoration: const InputDecoration(labelText: 'Name'),
+//                 ),
+//                 TextField(
+//                   keyboardType:
+//                       const TextInputType.numberWithOptions(decimal: true),
+//                   controller: _priceController,
+//                   decoration: const InputDecoration(
+//                     labelText: 'Price',
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 ElevatedButton(
+//                   child: Text(action == 'create' ? 'Create' : 'Update'),
+//                   onPressed: () async {
+//                     final String? name = _nameController.text;
+//                     final double? price =
+//                         double.tryParse(_priceController.text);
+//                     if (name != null && price != null) {
+//                       if (action == 'create') {
+//                         // Persist a new product to Firestore
+//                         await _productss.add({"name": name, "price": price});
+//                       }
+
+//                       if (action == 'update') {
+//                         // Update the product
+//                         await _productss
+//                             .doc(documentSnapshot!.id)
+//                             .update({"name": name, "price": price});
+//                       }
+
+//                       // Clear the text fields
+//                       _nameController.text = '';
+//                       _priceController.text = '';
+
+//                       // Hide the bottom sheet
+//                       Navigator.of(context).pop();
+//                     }
+//                   },
+//                 )
+//               ],
+//             ),
+//           );
+//         });
+//   }
+
+//   // Deleteing a product by id
+//   Future<void> _deleteProduct(String productId) async {
+//     await _productss.doc(productId).delete();
+
+//     // Show a snackbar
+//     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+//         content: Text('You have successfully deleted a product')));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Kindacode.com'),
+//       ),
+//       // Using StreamBuilder to display all products from Firestore in real-time
+//       body: StreamBuilder(
+//         stream: _productss.snapshots(),
+//         builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+//           if (streamSnapshot.hasData) {
+//             return ListView.builder(
+//               itemCount: streamSnapshot.data!.docs.length,
+//               itemBuilder: (context, index) {
+//                 final DocumentSnapshot documentSnapshot =
+//                     streamSnapshot.data!.docs[index];
+//                 return Card(
+//                   margin: const EdgeInsets.all(10),
+//                   child: ListTile(
+//                     title: Text(documentSnapshot['name']),
+//                     subtitle: Text(documentSnapshot['price'].toString()),
+//                     trailing: SizedBox(
+//                       width: 100,
+//                       child: Row(
+//                         children: [
+//                           // Press this button to edit a single product
+//                           IconButton(
+//                               icon: const Icon(Icons.edit),
+//                               onPressed: () =>
+//                                   _createOrUpdate(documentSnapshot)),
+//                           // This icon button is used to delete a single product
+//                           IconButton(
+//                               icon: const Icon(Icons.delete),
+//                               onPressed: () =>
+//                                   _deleteProduct(documentSnapshot.id)),
+//                         ],
+//                       ),
+//                     ),
+//                   ),
+//                 );
+//               },
+//             );
+//           }
+
+//           return const Center(
+//             child: CircularProgressIndicator(),
+//           );
+//         },
+//       ),
+//       // Add new product
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => _createOrUpdate(),
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
